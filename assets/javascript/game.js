e2dc13ae0126a8c9fcefe7fbf3ad7cf023c4ffa2
Eric Nelson II <nelsoneric2011@gmail.com>
2018-10-12 13:36:16 -0700

    Styling css. Approaching a crash themed crystal game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,93 @@
 var targetNumber;
-var loss; 
-var win;
+var lost = 0;
+var win = 0;
+var originalNumber = 0;
 
-// random number from  19- 120
-targetNumber = Math.floor(Math.random() * 101) +19;
-// console.log(targetNumber);
 
-// display the "target number" on html
-$("#targetNumber").html('Target Number: ' + targetNumber);
+// start the game
+var resetAndStart = function () {
+    // emptry crystal value to get new number each round and prevents doubles
+    $(".crystals").empty();
+    var images = [
+        "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/6/6a/Free_Crystal.png/revision/latest?cb=20151121235051",
+        "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/1/19/Crystal_rare.png/revision/latest?cb=20151122000058",
+        "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/1/1c/2-Star_Crystal.png/revision/latest?cb=20150825213642",
+        "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/8/8d/Daily_Crystal.png/revision/latest?cb=20150825213624"
+    ]
+    // random number from  19- 120
+    targetNumber = Math.floor(Math.random() * 101) + 19;
+    // console.log(targetNumber);
 
-// create for loop for each crystal box and create a div for each crystal.
-// for loop to make random number 1-12 in 4 times
-for (var i = 0; i < 4; i++) {
+    // display the "target number" on html
+    $("#targetNumber").html('Target Number: ' + targetNumber);
 
-    // random number up to 12
-     var random = Math.floor(Math.random() * 11) +1;
-    // console.log(random);
-    
-    // crystal div for iamge to be placed in 
-    var crystal = $("<div>");
+    // create for loop for each crystal box and create a div for each crystal.
+    // for loop to make random number 1-12 in 4 times
+    for (var i = 0; i < 4; i++) {
+
+        // random number up to 12
+        var random = Math.floor(Math.random() * 11) + 1;
+        // console.log(random);
+
+        // crystal div for iamge to be placed in 
+        var crystal = $("<div>");
+        // assign crystals a value with random hw
         crystal.attr({
             "class": 'crystal',
             "random-number": random
         });
-
-    $(".crystals").append(crystal);
+        crystal.css({
+            "background-images": "url" + (images[i]) + "",
+            "background":"pink",
+        });
+        $(".crystals").append(crystal);
+    }
+    $("#originalNumber").html("Number Count: " + originalNumber);
 }
 
-$(".crystal").on("click", function() {
-    console.log($(this).attr("random-number")); 
+resetAndStart();
+var reset = function () {
+
+}
+
+// when clicking one of the four crystals, should add the previous result      
+$(document).on("click", ".crystal", function () {
+
+    var num = parseInt($(this).attr("random-number"));
+    // able add results together to get tagert number
+    originalNumber += num;
+
+    $("#originalNumber").html("Number Count: " + originalNumber);
+    console.log(originalNumber);
+
+    // if user user score goes above ta rget number, you lose 
+    if (originalNumber > targetNumber) {
+        //console, console.log("you lose");
+
+        lost++;
+
+        $("#lost").html("Loss: " + lost);
+
+        $("#originalNumber").html("Number Count: " + originalNumber);
+
+        originalNumber = 0;
+
+        resetAndStart();
+    }
+    // if user score equals target number, you wim
+    else if (originalNumber === targetNumber) {
+        // console.log("you win")
+
+        win++;
+
+        $("#win").html("Win: " + win);
+
+        $("#originalNumber").html("Number Count: " + originalNumber);
+
+        originalNumber = 0;
+
+        resetAndStart();
+    }
+
+    // console.log(originalNumber);
 });
